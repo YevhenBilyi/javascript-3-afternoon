d95dd65bf1762f0266660b97713356561ffac4e8
YevhenBilyi <leugenewhitel@gmail.com>
2018-03-21 14:40:08 -0600

    Done

--- destructuring.js
+++ destructuring.js
@@ -22,7 +22,7 @@ var carDetails = {
 */
 
 //Code Here
-
+var {color, make, model, year}=carDetails;
 
 
 ////////// PROBLEM 2 //////////
@@ -35,7 +35,7 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
-  
+  var {firstName, lastName, title}=obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -54,7 +54,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function totalPopulation(obj){
+  var {utah, california, texas, arizona}=obj;
+  return utah+california+texas+arizona;
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -68,7 +71,11 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function ingredients(obj){
+  var {carb, fat, protein}=obj;
+  var arr=[carb, fat, protein];
+  return arr;
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -86,7 +93,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function largeNumbers({first, second,third}){
+  var arr=[first,second,third];
+  return arr.sort((a,b)=>a-b)[0];
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -98,5 +108,9 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function numberGroups({a,b,c}){
+ if(a.length>b.length&&a.length>c.length) return a;
+ else if(b.length>a.length&& b.length>c.length) return b;
+ else return c;
+}
 
